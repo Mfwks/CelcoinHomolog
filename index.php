a1a00e53a08e96c975f404fa209f81bdcb6cd8fb
Eskelsen <dev@microframeworks.com>
2026-03-19 21:26:17 +0000

    Retrieve the code for the repository

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+# Index Dock Mock
+
+$in = explode('/',$_SERVER['REQUEST_URI'], 4);
+
+$call = end($in);
+
+if (!$call) {
+    header('Content-Type: application/json');
+    exit('{"error": "Nenhum endpoint informado"}');
+}
+
+include __DIR__ . '/functions.php';
+
+$content = file_get_contents('php://input');
+$data = json_decode($content, 1);
+
+$fn = [
+    'pix/v1/dict/v2/key' => 'consultarChaveAntigo',
+    'enviar-pix' => 'enviarPix',
+    'baas-wallet-transactions-webservice/v1/pix/payment' => 'enviarPix'
+];
+
+$function = $fn[$call] ?? false;
+
+if (!$function) {
+    if (preg_match('#^([^/]+)/v1/pix/dict/entry/external/([^/]+)\?key=([^/]+)$#', $call, $matches)) {
+        // echo 'matches' . PHP_EOL;
+        // var_dump($matches);
+        $email = $matches[1] ?? null;
+        $account = $matches[2] ?? null;
+        if ($email && $account) {
+            $function = 'consultarChave';
+            $data['email'] = $email;
+            $data['account'] = $account;
+        }
+    }
+}
+
+if ($function==false || !function_exists(($function))) {
+    header('Content-Type: application/json');
+    http_response_code(404);	
+    $log = '# ' . $call . "\n\n" . json_encode($data, JSON_PRETTY_PRINT) . "\n\n";
+    file_put_contents(__DIR__ . '/logs/' . date('YmdHi') . '.log', $log, FILE_APPEND);
+    exit('{"error": "Função inexistente para ' . $call. '"}');
+}
+
+$response = call_user_func($function,$data);
+
+$log = '# ' . $call . "\n\n" . "function: $function\n\n" . json_encode($data, JSON_PRETTY_PRINT) . "\n\n" . $response . "\n\n";
+
+file_put_contents(__DIR__ . '/logs/' . date('YmdHi') . '.log', $log, FILE_APPEND);
+
+header('Content-Type: application/json');
+exit($response);
